7e92c05eb0b02a2fd04dcd0d282492eac9ae93a2
mateoer <102526524+mateoer@users.noreply.github.com>
2022-08-19 22:46:59 -0400

    Spring Security allows POST requests

--- src/main/java/root/service/security/SecurityConfiguration.java
+++ src/main/java/root/service/security/SecurityConfiguration.java
@@ -1,21 +1,47 @@
 package root.service.security;
 
-//@Configuration
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
-//	@Bean
-//	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//	return authenticationConfiguration.getAuthenticationManager();
-//	}
-//	
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeHttpRequests((authz) -> authz
-//                .anyRequest().authenticated()
-//            )
-//				.httpBasic(/* withDefaults() */) ;
-//        return http.build();
-//    }
+    @Value("${spring.security.debug:false}")
+    boolean securityDebug;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+          .disable()
+          .authorizeRequests()
+//          .antMatchers(HttpMethod.DELETE)
+//          .hasRole("ADMIN")
+//          .antMatchers("/admin/**")
+//          .hasAnyRole("ADMIN")
+//          .antMatchers("/user/**")
+//          .hasAnyRole("USER", "ADMIN")
+//          .antMatchers("/login/**")
+//          .anonymous()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic() ;
+//          .and()
+//          .sessionManagement()
+//          .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.debug(securityDebug)
+//          .ignoring()
+//          .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
+//    }
 }
