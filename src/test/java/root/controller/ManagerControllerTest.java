af071404e5a8c3e71a26cf5435f133723cfd1094
mateoer <102526524+mateoer@users.noreply.github.com>
2022-09-03 22:39:47 -0400

    test imports

--- src/test/java/root/controller/ManagerControllerTest.java
+++ src/test/java/root/controller/ManagerControllerTest.java
@@ -3,7 +3,7 @@ package root.controller;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
